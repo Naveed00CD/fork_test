09c3a0fb3e995c564445bd48b19defa2376a31e2
Naveed00CD <na982498@gmail.com>
2025-04-09 21:08:15 +0530

    this time [200~git pull --no-rebase

--- app.py
+++ app.py
@@ -6,8 +6,13 @@ def lambda_handler(event, context):
   response = client.run_instances(
     ImageId='ami-0614680123427b75e',
     InstanceType='t2.micro',
+<<<<<<< HEAD
     KeyName='from ec_2 8:54 test 2',
     MaxCount=2,
+=======
+    KeyName='from ec_2',
+    MaxCount=2from git terminal,
+>>>>>>> 232c82a (test2 termianl)
     MinCount=2
 )
 
