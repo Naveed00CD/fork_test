29705e6da2800515b6bdbf165881336299f8be3a
root <root@ip-10-0-197-75.ap-south-1.compute.internal>
2025-04-09 15:11:15 +0000

    from ec2

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ def lambda_handler(event, context):
   response = client.run_instances(
     ImageId='ami-0614680123427b75e',
     InstanceType='t2.micro',
-    KeyName='aws',
+    KeyName='from ec_2',
     MaxCount=2,
     MinCount=2
 )
